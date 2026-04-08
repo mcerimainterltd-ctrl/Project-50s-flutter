f58f1554a71f307900744f5c0440b496587d18b1
Gibson <gibsonagbor@gmail.com>
2026-04-09 00:42:25 +0100

    BUILD 83: Prioritizing Call Connection Handshake

--- lib/core/services/webrtc_service.dart
+++ lib/core/services/webrtc_service.dart
@@ -12,20 +12,16 @@ final webRTCServiceProvider = Provider((ref) {
 
 class WebRTCService {
   final WebRTCSocketService _socket;
-  MediaStream? localStream;
   RTCPeerConnection? _pc;
+  MediaStream? localStream;
   
-  bool isAudioMuted = false;
-  bool isVideoMuted = false;
-  bool isLoudspeakerOn = false;
-
   final _callStateController = StreamController<CallState>.broadcast();
-  final _remoteStreamController = StreamController<MediaStream>.broadcast();
   final _incomingCallCtrl = StreamController<bool>.broadcast();
+  final _remoteStreamController = StreamController<MediaStream>.broadcast();
 
   Stream<CallState> get callState => _callStateController.stream;
-  Stream<MediaStream> get remoteStream$ => _remoteStreamController.stream;
   Stream<bool> get onIncomingCall => _incomingCallCtrl.stream;
+  Stream<MediaStream> get remoteStream$ => _remoteStreamController.stream;
   Stream<String> get callTimer => Stream.periodic(const Duration(seconds: 1), (i) => "00:${i.toString().padLeft(2, '0')}");
 
   WebRTCService(this._socket) {
@@ -33,39 +29,54 @@ class WebRTCService {
   }
 
   void _listenToSocket() {
-    // THIS activates the notification pop-up
     _socket.onCallOffer.listen((data) {
       _incomingCallCtrl.add(true);
       _callStateController.add(CallState.incoming);
     });
+
+    _socket.onMakeAnswer.listen((data) async {
+      if (_pc != null) {
+        var answer = data['answer'];
+        await _pc!.setRemoteDescription(RTCSessionDescription(answer['sdp'], answer['type']));
+        _callStateController.add(CallState.active);
+      }
+    });
+
+    _socket.onIceCandidate.listen((data) {
+      if (_pc != null) {
+        var cand = data['candidate'];
+        _pc!.addCandidate(RTCIceCandidate(cand['candidate'], cand['sdpMid'], cand['sdpMLineIndex']));
+      }
+    });
   }
 
-  Future<void> startCall(String remoteUserId, dynamic isVideoInput) async {
-    bool isVideo = isVideoInput == true || isVideoInput == 'video';
+  Future<void> startCall(String userId, dynamic type) async {
     _callStateController.add(CallState.outgoing);
-    
-    localStream = await navigator.mediaDevices.getUserMedia({
-      'audio': true,
-      'video': isVideo ? {'facingMode': 'user'} : false,
-    });
-    
     _pc = await createPeerConnection({'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]});
-    localStream!.getTracks().forEach((track) => _pc!.addTrack(track, localStream!));
     
+    _pc!.onIceCandidate = (candidate) => _socket.sendIceCandidate(userId, candidate);
+    _pc!.onTrack = (event) => _remoteStreamController.add(event.streams[0]);
+
+    localStream = await navigator.mediaDevices.getUserMedia({'audio': true, 'video': type == 'video'});
+    localStream!.getTracks().forEach((track) => _pc!.addTrack(track, localStream!));
+
     RTCSessionDescription offer = await _pc!.createOffer();
     await _pc!.setLocalDescription(offer);
-    _socket.sendCallOffer(remoteUserId, offer, isVideo ? 'video' : 'voice');
+    _socket.sendCallOffer(userId, offer, type.toString());
   }
-
-  void toggleAudio() => isAudioMuted = !isAudioMuted;
-  void toggleVideo() => isVideoMuted = !isVideoMuted;
-  Future<void> toggleSpeaker() async => isLoudspeakerOn = !isLoudspeakerOn;
-  Future<void> toggleCamera() async {}
 
   Future<void> endCall() async {
     _callStateController.add(CallState.ended);
-    await localStream?.dispose();
     await _pc?.close();
     _pc = null;
   }
+
+  // UI Stubs to keep build stable
+  bool isAudioMuted = false;
+  bool isVideoMuted = false;
+  bool isLoudspeakerOn = false;
+  void toggleAudio() {}
+  void toggleVideo() {}
+  Future<void> toggleSpeaker() async {}
+  Future<void> toggleCamera() async {}
 }
--- lib/core/services/webrtc_socket_service.dart
+++ lib/core/services/webrtc_socket_service.dart
@@ -1,21 +1,45 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'socket_service.dart'; // Assuming your base socket logic is here
 
-final webRTCSocketServiceProvider = Provider((ref) => WebRTCSocketService());
+final webRTCSocketServiceProvider = Provider((ref) {
+  final socketService = ref.watch(socketServiceProvider);
+  return WebRTCSocketService(socketService);
+});
 
 class WebRTCSocketService {
-  void connect(String userId) {}
+  final SocketService _baseSocket;
   
-  // The missing methods the compiler is asking for:
+  WebRTCSocketService(this._baseSocket);
+
+  // Streams for the WebRTC Service to listen to
+  Stream<dynamic> get onCallOffer => _baseSocket.getResponse('call-user');
+  Stream<dynamic> get onMakeAnswer => _baseSocket.getResponse('make-answer');
+  Stream<dynamic> get onIceCandidate => _baseSocket.getResponse('ice-candidate');
+
   void sendCallOffer(String to, RTCSessionDescription offer, String type) {
-    // Logic to emit 'call-offer' to your Node.js server
+    _baseSocket.emit('call-user', {
+      'recipientId': to,
+      'offer': {'sdp': offer.sdp, 'type': offer.type},
+      'callType': type,
+    });
+  }
+
+  void sendAnswer(String to, RTCSessionDescription answer) {
+    _baseSocket.emit('make-answer', {
+      'to': to,
+      'answer': {'sdp': answer.sdp, 'type': answer.type},
+    });
   }
 
   void sendIceCandidate(String to, RTCIceCandidate candidate) {
-    // Logic to emit 'ice-candidate' to your Node.js server
+    _baseSocket.emit('ice-candidate', {
+      'to': to,
+      'candidate': {
+        'candidate': candidate.candidate,
+        'sdpMid': candidate.sdpMid,
+        'sdpMLineIndex': candidate.sdpMLineIndex,
+      },
+    });
   }
-
-  Stream<dynamic> get onCallOffer => const Stream.empty();
-  Stream<dynamic> get onIceCandidate => const Stream.empty();
-  Stream<dynamic> get onMakeAnswer => const Stream.empty();
 }
