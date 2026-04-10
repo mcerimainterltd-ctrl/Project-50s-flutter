b52bcf7988e92cfe57767668f750e7b205be04ab
Gibson <gibsonagbor@gmail.com>
2026-04-10 11:06:00 +0100

    FIX 168: add TURN servers - fix ICE failure behind NAT (root cause of no remote media)

--- lib/core/services/webrtc_service.dart
+++ lib/core/services/webrtc_service.dart
@@ -108,7 +108,25 @@ class WebRTCService {
 
   Future<void> _setup(bool v) async {
     _pc = await createPeerConnection({
-      'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}],
+      'iceServers': [
+          {'urls': 'stun:stun.l.google.com:19302'},
+          {'urls': 'stun:stun1.l.google.com:19302'},
+          {
+            'urls': 'turn:openrelay.metered.ca:80',
+            'username': 'openrelayproject',
+            'credential': 'openrelayproject',
+          },
+          {
+            'urls': 'turn:openrelay.metered.ca:443',
+            'username': 'openrelayproject',
+            'credential': 'openrelayproject',
+          },
+          {
+            'urls': 'turn:openrelay.metered.ca:443?transport=tcp',
+            'username': 'openrelayproject',
+            'credential': 'openrelayproject',
+          },
+        ],
       'sdpSemantics': 'unified-plan'
     });
     
