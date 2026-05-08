648268d0bbc4ff6a6182e2106528a0a9a2327dcc
Gibson <gibsonagbor@gmail.com>
2026-05-08 10:31:04 +0100

    feat: iOS native CallKit, VoIP push, FCM - AppDelegate, CallKitService, Info.plist

---
+++ ios/Runner/CallKitService.swift
@@ -0,0 +1,70 @@
+import CallKit
+import AVFoundation
+
+class CallKitService: NSObject, CXProviderDelegate {
+
+    static let shared = CallKitService()
+    private let provider: CXProvider
+    private let callController = CXCallController()
+    private var currentCallUUID: UUID?
+
+    override init() {
+        let config = CXProviderConfiguration()
+        config.localizedName = "XamePage"
+        config.supportsVideo = true
+        config.maximumCallsPerCallGroup = 1
+        config.supportedHandleTypes = [.generic]
+        provider = CXProvider(configuration: config)
+        super.init()
+        provider.setDelegate(self, queue: nil)
+    }
+
+    func reportIncomingCall(callerName: String, callType: String) {
+        let uuid = UUID()
+        currentCallUUID = uuid
+        let update = CXCallUpdate()
+        update.remoteHandle = CXHandle(type: .generic, value: callerName)
+        update.hasVideo = callType == "video"
+        update.localizedCallerName = callerName
+        update.supportsHolding = false
+        update.supportsGrouping = false
+        update.supportsUngrouping = false
+        update.supportsDTMF = false
+        provider.reportNewIncomingCall(with: uuid, update: update) { error in
+            if let error = error {
+                print("[CallKit] Error: \(error)")
+            }
+        }
+    }
+
+    func endCall() {
+        guard let uuid = currentCallUUID else { return }
+        let transaction = CXTransaction(action: CXEndCallAction(call: uuid))
+        callController.request(transaction) { _ in }
+        currentCallUUID = nil
+    }
+
+    func providerDidReset(_ provider: CXProvider) {
+        currentCallUUID = nil
+    }
+
+    func provider(_ provider: CXProvider, perform action: CXAnswerCallAction) {
+        NotificationCenter.default.post(name: Notification.Name("CallAnswered"), object: nil)
+        action.fulfill()
+    }
+
+    func provider(_ provider: CXProvider, perform action: CXEndCallAction) {
+        NotificationCenter.default.post(name: Notification.Name("CallEnded"), object: nil)
+        currentCallUUID = nil
+        action.fulfill()
+    }
+
+    func provider(_ provider: CXProvider, didActivate audioSession: AVAudioSession) {
+        try? audioSession.setCategory(.playAndRecord, mode: .voiceChat, options: [.allowBluetooth])
+        try? audioSession.setActive(true)
+    }
+
+    func provider(_ provider: CXProvider, didDeactivate audioSession: AVAudioSession) {
+        try? audioSession.setActive(false)
+    }
+}
