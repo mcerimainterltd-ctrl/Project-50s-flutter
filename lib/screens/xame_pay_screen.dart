fd5a8481a6164a12922d054d3fd1af6a2051aaa9
Gibson <gibsonagbor@gmail.com>
2026-06-05 12:46:46 +0100

    Fix 591: add push_service import to xame_pay_screen

--- lib/screens/xame_pay_screen.dart
+++ lib/screens/xame_pay_screen.dart
@@ -8,6 +8,7 @@ import 'package:share_plus/share_plus.dart';
 import 'package:screenshot/screenshot.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
+import '../core/services/push_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../core/services/socket_service.dart';
