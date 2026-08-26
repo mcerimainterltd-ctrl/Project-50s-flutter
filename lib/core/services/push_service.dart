aaab8111ae852b44a18318a820ff962e2501355a
Gibson <gibsonagbor@gmail.com>
2026-08-26 23:03:51 +0100

    Fix 888: Log incoming push data for debugging

--- lib/core/services/push_service.dart
+++ lib/core/services/push_service.dart
@@ -1,3 +1,4 @@
+import 'package:get/get.dart';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -142,7 +143,7 @@ class PushService {
   void _handleForeground(RemoteMessage msg) {
     final data = msg.data;
     final type = data['type'];
-    if (type == 'incoming_call' || type == 'incoming-call') { Get.toNamed('/incoming-call'); return; }
+    if (type == 'incoming_call' || type == 'incoming-call') { Get.toNamed('/incoming-call', arguments: data); return; }
     if (type == 'message') {
       final settings = SettingsNotifier.currentSettings;
       if (!settings.msgSound && !settings.msgVibration) return;
