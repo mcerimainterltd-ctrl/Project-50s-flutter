a26768ca743b2ddb90be02853f5a01516cb09dbf
Gibson <gibsonagbor@gmail.com>
2026-09-03 01:25:34 +0100

    Fix 1717: Fix missing auth_service import in spaces_list_screen

--- lib/features/spaces/screens/spaces_list_screen.dart
+++ lib/features/spaces/screens/spaces_list_screen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../../../core/config/constants.dart';
+import '../../../core/services/auth_service.dart';
 
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
